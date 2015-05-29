e0332733c61593d2fa041e1e64175711f1f7aa25
7sun <pakrouse@gmail.com>
2015-05-29 12:37:18 -0700

    adds show product test

---
+++ app/assets/javascripts/product.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+
+  var showProduct = function(){
+    $.get('/products/ruby-on-rails-bag', function(data){
+      console.log("calls showProduct");
+      // console.log(data);
+      $('#products')
+      .prepend('<h1>HELLO THERE!</h1>');
+    });
+  }
+
+// });
