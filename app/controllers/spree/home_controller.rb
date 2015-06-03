4fdc692ddd4a52aa469c610d9c2c08590fc673aa
7sun <pakrouse@gmail.com>
2015-06-03 16:44:19 -0700

    adds inquiry model and connects inquiry form to DB

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -16,12 +16,28 @@ module Spree
       data = resp["data"]
       data.take(6).each do |obj|
         @images << obj["images"]["low_resolution"]["url"]
+      end
 
       # Cart logic for order. Comes from Spree OrdersController edit action
-        @order = current_order || Order.incomplete.find_or_initialize_by(guest_token: cookies.signed[:guest_token])
-        associate_user
-      end
-      
+      @order = current_order || Order.incomplete.find_or_initialize_by(guest_token: cookies.signed[:guest_token])
+      associate_user
+
+      # Sets up logic to create new inquiry from form
+      @inquiry = Inquiry.new
     end
+
+    def create_inquiry
+      @inquiry = Inquiry.new(params.require(:inquiry).permit(:name, :email, :datetime, :budget, :message, :customer_type))
+      if @inquiry.valid?
+        if @inquiry.save
+          flash[:confirm] = "Your inquiry has been sent! We will contact you shortly."
+          redirect_to "/"
+        end
+      else
+        flash[:error] = "You are missing a required field in your inquiry."
+        render :index
+      end
+    end
+
   end
 end
