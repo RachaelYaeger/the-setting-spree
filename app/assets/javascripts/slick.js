2c0d7ae45101212ade047127f19653164a844231
7sun <pakrouse@gmail.com>
2015-07-05 18:24:33 -0700

    changes slideshow autoplay speed to 5 sec

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -23,7 +23,7 @@ function ready() {
       appendArrows: '#studio-slider-count',
       dots: false,
       autoplay: true,
-      autoplaySpeed: 3000,
+      autoplaySpeed: 5000,
     });
     
     $('.slickness-products').slick({
