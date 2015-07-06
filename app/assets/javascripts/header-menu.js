aa8a9d2dbb20b1c165dca02064e6f2a0f6fd259a
7sun <pakrouse@gmail.com>
2015-07-05 20:13:56 -0700

    adds mobile nav

--- app/assets/javascripts/header-menu.js
+++ app/assets/javascripts/header-menu.js
@@ -1,17 +1,52 @@
 $(document).ready(function() {
-  // var menuToggle = $('#js-centered-navigation-menu-btn').unbind();
   
-  $(document).on('mouseenter', '#js-centered-navigation-menu-btn', function(e) {
-    e.preventDefault();
-    $('#js-centered-navigation-menu').slideDown(function(){
-      if($('#js-centered-navigation-menu').is(':hidden')) {
-        $('#js-centered-navigation-menu').removeAttr('style');
+  if (Modernizr.touch){
+    
+    // Hides and shows mobile menu section with a sliding animation on Menu click
+    $(document).on('click', '#js-centered-navigation-menu-btn', function(e) {
+      e.preventDefault();
+      if ($('#mobile-menu').is(':visible')) {
+        $('#mobile-menu, #space-holder').slideUp();
+      }
+      else {
+        $('#mobile-menu, #space-holder').slideDown();
       }
     });
-  });
 
-  $(document).on('mouseleave', '#menu-container' , function(){
-    $('#js-centered-navigation-menu').slideUp();
-  });
+    // Hides mobile menu when any nav link is clicked
+    $(document).on('click', '.mobile-nav-link', function(e){
+      e.preventDefault();
+      var currentAnchor = $(this).find("a").attr('href');
+      if ($(this).text() == "Journal"){
+        window.location = currentAnchor;
+      }
+      $('#mobile-menu, #space-holder').slideUp(function(){
+        $.smoothScroll({
+              scrollTarget: currentAnchor,
+              offset: -60,
+              speed: 800
+            });
+        return false;
+      });
+    })
+
+  } else {
+
+    $(document).on('mouseenter', '#js-centered-navigation-menu-btn', function(e) {
+      e.preventDefault();
+      $('#js-centered-navigation-menu').slideDown(function(){
+        if($('#js-centered-navigation-menu').is(':hidden')) {
+          $('#js-centered-navigation-menu').removeAttr('style');
+        }
+      });
+    });
+
+    $(document).on('mouseleave', '#menu-container' , function(){
+      $('#js-centered-navigation-menu').slideUp();
+    });
+
+  }
+
+
   
 });
