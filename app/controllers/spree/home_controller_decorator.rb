5e13ae250b8db38fba82a2963bdd6cbae25b3fa7
aaronzdavis <aaronzdavis@gmail.com>
2016-06-29 16:42:27 -0700

    User taxons for homepage products

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,11 +2,9 @@ Spree::HomeController.class_eval do
   before_filter :set_site_settings, :only => :index
 
   def index
-    @searcher = build_searcher(params.merge(include_images: true))
-    @products = @searcher.retrieve_products.order("grid_position ASC")
-    # @taxon = Spree::Taxon.find_by(name: "Home")
-    # @searcher = build_searcher(params.merge(include_images: true, taxon: @taxon))
-    # @products = @searcher.retrieve_products()
+    taxon = Spree::Taxon.find_by(name: "Home")
+    @searcher = build_searcher(params.merge(include_images: true, taxon: taxon))
+    @products = @searcher.retrieve_products()
     @taxonomies = Spree::Taxonomy.includes(root: :children)
   end
 
