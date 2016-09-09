c1c652c331289d70b49ff06d5e96a09d7cb227f1
aaronzdavis <aaronzdavis@gmail.com>
2016-09-09 09:43:16 -0700

    revert sendgrid

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,23 +1,12 @@
-# ActionMailer::Base.smtp_settings = {
-#   port:           587,
-#   address:        'smtp.mandrillapp.com',
-#   user_name:      ENV['MANDRILL_USERNAME'],
-#   password:       ENV['MANDRILL_API_KEY'],
-#   domain:         ENV['DOMAIN'],
-#   authentication: 'login',
-#   enable_starttls_auto: true
-# }
-# ActionMailer::Base.delivery_method = :smtp
-
-
 Rails.application.configure do
   config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    port:           587,
+    address:        'smtp.mandrillapp.com',
+    user_name:      ENV['MANDRILL_USERNAME'],
+    password:       ENV['MANDRILL_API_KEY'],
+    domain:         ENV['DOMAIN'],
+    authentication: 'login',
+    enable_starttls_auto: true
   }
+  config.action_mailer.delivery_method = :smtp
 end
